caa74ff1b391cd89511170c3028f79207b71b852
ShinChanghak <csg1353@naver.com>
2023-06-22 23:13:22 +0900

    0622 login/register구현완료

---
+++ BE/Refactoring/src/main/java/com/sas/refactoring/user/service/UserService.java
@@ -0,0 +1,13 @@
+package com.sas.refactoring.user.service;
+
+import com.sas.refactoring.user.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    UserDto checkLogin(List<String> data) throws Exception;
+
+    void registerUser(List<String> data) throws Exception;
+
+    boolean checkDuplicate(List<String> data) throws Exception;
+}
--- FE/vue-project/src/views/user/UserLogin.vue
+++ FE/vue-project/src/views/user/UserLogin.vue
@@ -1,6 +1,8 @@
 
+
 <template>
-    <div>
+  <div>
+    <div class="outer-container">
       <h2>Login</h2>
       <input type="text" v-model="id" placeholder="Id" />
       <input type="password" v-model="password" placeholder="Password" />
@@ -13,10 +15,11 @@
       <router-link to="/">돌아가기</router-link>
 
     </div>
+  </div>
   </template>
   
   <script>
-  import { mapActions } from 'vuex';
+  import { mapActions, mapState } from 'vuex';
   export default {
     data() {
       return {
@@ -25,23 +28,48 @@
         loginError: '',
       };
     },
+    computed: {
+      ...mapState("userStore", ["loggedIn"]),
+    },
     methods: {
       ...mapActions("userStore", ["login"]),
-      loginCheck() {
-        this.login({id : this.id, password : this.password});
+      async loginCheck() {
+    try {
+      await this.login({id : this.id, password : this.password})
+    } catch (error) {
+      console.error(error);
+    }
+      if(this.loggedIn) {
+        this.$router.push('/');
+      }
+      else {
+        this.loginError = 'Invalid username or password.';
+      }
 
-        // if (this.username === 'admin' && this.password === 'password') {
-        //   this.$store.commit('login'); // Vuex store의 'login' mutation을 호출하여 로그인 상태를 변경합니다.
-        //   this.loginError = '';
-        // } else {
-        //   this.loginError = 'Invalid username or password.';
-        // }
       },
     },
   };
   </script>
   
   <style scoped>
+  .outer-container {
+    
+    display: flex;
+    flex-direction: column;
+
+    max-height: 350px;
+    max-width: 550px;
+
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0 auto;
+
+    border-radius: 10px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2); /* 그림자 효과 추가 */
+    background-color: #ffffff;
+    border: 1px solid #ccc; 
+  }
   .error {
     color: red;
   }
