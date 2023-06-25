04f53b8aa93d16b691d95fcd1ca2076bae88be1e
신창학 <csg1353@naver.com>
2023-06-25 17:52:43 +0900

    ignore

--- BE/Refactoring/src/main/java/com/sas/refactoring/freeBoard/controller/FreeBoardController.java
+++ BE/Refactoring/src/main/java/com/sas/refactoring/freeBoard/controller/FreeBoardController.java
@@ -1,9 +1,13 @@
 package com.sas.refactoring.freeBoard.controller;
 
+import com.sas.refactoring.freeBoard.dto.FreeBoardDto;
 import com.sas.refactoring.freeBoard.service.FreeBoardService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/freeboard")
 @CrossOrigin("*")
@@ -13,6 +17,11 @@ public class FreeBoardController {
 
     public FreeBoardController(FreeBoardService freeBoardService) {
         this.freeBoardService = freeBoardService;
+    }
+
+    @GetMapping
+    public List<FreeBoardDto> showFreeBoard() throws Exception {
+        return freeBoardService.showFreeBoardList();
     }
 
 }
--- BE/Refactoring/src/main/java/com/sas/refactoring/freeBoard/service/FreeBoardService.java
+++ BE/Refactoring/src/main/java/com/sas/refactoring/freeBoard/service/FreeBoardService.java
@@ -1,6 +1,10 @@
 package com.sas.refactoring.freeBoard.service;
 
+import com.sas.refactoring.freeBoard.dto.FreeBoardDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface FreeBoardService {
+    List<FreeBoardDto> showFreeBoardList() throws Exception;
 }
